2f8d90648c8950c0f236232506c2360468a877ff
SamMoher <smohereight@gmail.com>
2019-03-12 11:49:49 -0400

    added random image generation to gallery

--- web/js/main.js
+++ web/js/main.js
@@ -10,7 +10,7 @@ async function loadMenu(){
     // desserts
     loadMenuItems(8, "desserts");
     // gallery
-    loadMenuItems(8, "gallery")
+    loadGallery(8, "gallery");
 }
 
 
@@ -25,7 +25,9 @@ async function loadMenuItems(num, whereTo) {
 
             for (var i = 0; i < myJson.menu_items.length; i++) {
                 var title1 = myJson.menu_items[i].description;
+                // splitting first 2 indexes(words) off of the menu description and turning them into the card title
                 var arr = title1.split(" ");
+                // creating a random number whole number between 8 and 19 for price in card sub title
                 var p = Math.floor(Math.random() * 12) + 8;
                 app += "<div class='col-sm-6 col-md-4 col-lg-3'>";
                 app += "<div class='card' id='item-" + i + "'>";
@@ -38,4 +40,19 @@ async function loadMenuItems(num, whereTo) {
             document.getElementById(whereTo).innerHTML = app;
         });
         
+}
+
+function getUrl(i){
+    return "https://source.unsplash.com/collection/271352/25" + i + "x25" + i;
+}
+
+function loadGallery(num, whereTo) {
+    var pic = "";
+    for (var i = 0; i < num; i++){
+        pic += "<div class='col-sm-6 col-md-4 col-lg-3'>";
+        pic += "<img id='gallery-imgs' class='img-fluid' src='" + getUrl(i) + "'>";
+       pic += "</div>";
+    }
+    console.log(pic);
+    document.getElementById(whereTo).innerHTML = pic;
 }
