37df67e3d2ecc0dddec94a536844631cd9907dfe
Unbroken Technology <s.albert.staton@gmail.com>
2019-03-15 15:04:27 -0400

    notes added

--- web/js/main.js
+++ web/js/main.js
@@ -13,8 +13,8 @@ document.onreadystatechange = function () {
 }
 
 // async this function to settle data at one time. Await allows this funtion to stop with out blocking the other promises until init
-// getMenu(#) pulls the first 8 stings from the array "https://entree-f18.herokuapp.com/v1/menu", and logs it to menu1 this repeats for each varible
-async function init(){
+// getMenu(8) pulls the first 8 stings from the array "https://entree-f18.herokuapp.com/v1/menu", and logs it to menu1 this repeats for each varible
+async function init(){        
     var menu1 = await getMenu(8);
     updateMenu("menu1_output", menu1);
 
@@ -29,12 +29,10 @@ async function init(){
 }
 
 // this  function pulls our Array data  from our API  and converts it into a string and then saves it to data and sets it to 
-// fetchResults
-
-
+// fetchResults then we return fetchResults to end the function
 async function getMenu(id) {
-    let url = 'https://entree-f18.herokuapp.com/v1/menu/' + id;
-    var d = await fetch(url)
+    let url = 'https://entree-f18.herokuapp.com/v1/menu/' + id; 
+    var id = await fetch(url)
     .then(status)
     .then(json)
     .then(function(data) {
@@ -47,19 +45,22 @@ async function getMenu(id) {
     return fetchResults;
 }
 
+//This function is what creates the list effect on my tabs in the menu.
 function updateMenu (menu_holder, menu){
     var output = document.getElementById(menu_holder);
     output.style.display = 'block';
     var str = "<ul>";
-    for(var i = 0; i < menu.menu_items.length; i++){
-      var menu_item = menu.menu_items[i];
+    // for(var i = 0; i < menu.menu_items.length; i++){        <--- this for loop starts at 0 in the Array for menu_holder, and loops for the length of the menu items until 
+    //   var menu_item = menu.menu_items[i];                      we land at the end of the four loop. 
       str += "<li>" + menu_item.description + "</li>";
     }
     str += "</ul>";
     output.innerHTML = str;
   }
   
-  //
+  
+  //This function checks the response time for our fetch function. If the time takes between 200ms-300ms the fetch function isn't working fast enough
+
   function status(response) {
       if (response.status >= 200 && response.status < 300) {
         return Promise.resolve(response)
@@ -72,89 +73,5 @@ function updateMenu (menu_holder, menu){
       return response.json()
     }
 
-
-
-// function getFood() {
-//     fetch('https://entree-f18.herokuapp.com/v1/menu')
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (myJson) {
-
-//             for (let i = 0; i < 8; i++) {
-
-//                 let food = JSON.stringify(myJson.menu_items[i].description);
-//                 food = food.slice(1, -1);
-
-//                 food += " $" + Math.round(Math.random() * 10000) / 100;
-//                 console.log(food);
-//                 if (food.charAt(food.length - 2) == ".") {
-//                     food += "0";
-//                 }
-//                 document.getElementById("foods" + i).innerHTML = food;
-//             }
-
-//         }
-//         ).catch(function (error) {
-//             console.log(error);
-
-//         });
-// }
-
-
-// function getApp() {
-//     fetch('https://entree-f18.herokuapp.com/v1/menu')
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (myJson) {
-
-//             for (let i = 0; i < 8; i++) {
-
-//                 let app = JSON.stringify(myJson.menu_items[i].description);
-//                 app = app.slice(1, -1);
-
-//                 app += " $" + Math.round(Math.random() * 10000) / 100;
-//                 console.log(app);
-//                 if (app.charAt(app.length - 2) == ".") {
-//                     app += "0";
-//                 }
-//                 document.getElementById("Apps" + i).innerHTML = app;
-//             }
-
-//         }
-//         ).catch(function (error) {
-//             console.log(error);
-
-//         });
-// }
-
-
-// // function getmCourse() {
-// //     fetch('https://entree-f18.herokuapp.com/v1/menu')
-// //         .then(function (response) {
-// //             return response.json();
-// //         })
-// //         .then(function (myJson) {
-
-// //             for (let i = 0; i < 8; i++) {
-
-// //                 let mCourse = JSON.stringify(myJson.menu_items[i].description);
-// //                 mCourse = mCourse.slice(1, -1);
-
-// //                 mCourse += " $" + Math.round(Math.random() * 10000) / 100;
-// //                 console.log(mCourse);
-// //                 if (mCourse.charAt(mCourse.length - 2) == ".") {
-// //                     mCourse += "0";
-// //                 }
-// //                 document.getElementById("mCourses" + i).innerHTML = mCourse;
-// //             }
-
-// //         }
-// //         ).catch(function (error) {
-// //             console.log(error);
-
-// //         });
-// // }
 
 
