8eba8c2e8dfaafed1790f9354c40022fc33d0251
Unbroken Technology <s.albert.staton@gmail.com>
2019-03-15 10:47:20 -0400

    varibles and async function

--- web/js/main.js
+++ web/js/main.js
@@ -1,48 +1,62 @@
-//pull information from api and call it.
 
 
+// Set varibles for objects to call
 
+var fetchResults = {};
+var menu1 = {};
+var menu2 = {};
+var menu3 = {};
+var menu4 = {};
 
+document.onreadystatechange = function () {
+    init();
+}
 
+// async this function to settle data at one time. Await allows this funtion to stop with out blocking the other promises until init
+// getMenu(#) pulls the first 8 stings from the array "https://entree-f18.herokuapp.com/v1/menu", and logs it to menu1 this repeats for each varible
+async function init(){
+    var menu1 = await getMenu(8);
+    updateMenu("menu1_output", menu1);
 
+    var menu2 = await getMenu(4);
+    updateMenu("menu2_output", menu2);
 
+    var menu3 = await getMenu(2);
+    updateMenu("menu3_output", menu3);
 
-
-
-function populateMenu() {
-    getApp();
-    getFood();
-    getmCourse();
-    getSides();
+    var menu4 = await getMenu(3);
+    updateMenu("menu4_output", menu4);
 }
 
 
-function getFood() {
-    fetch('https://entree-f18.herokuapp.com/v1/menu')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (myJson) {
 
-            for (let i = 0; i < 8; i++) {
 
-                let food = JSON.stringify(myJson.menu_items[i].description);
-                food = food.slice(1, -1);
+// function getFood() {
+//     fetch('https://entree-f18.herokuapp.com/v1/menu')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (myJson) {
 
-                food += " $" + Math.round(Math.random() * 10000) / 100;
-                console.log(food);
-                if (food.charAt(food.length - 2) == ".") {
-                    food += "0";
-                }
-                document.getElementById("foods" + i).innerHTML = food;
-            }
+//             for (let i = 0; i < 8; i++) {
 
-        }
-        ).catch(function (error) {
-            console.log(error);
+//                 let food = JSON.stringify(myJson.menu_items[i].description);
+//                 food = food.slice(1, -1);
 
-        });
-}
+//                 food += " $" + Math.round(Math.random() * 10000) / 100;
+//                 console.log(food);
+//                 if (food.charAt(food.length - 2) == ".") {
+//                     food += "0";
+//                 }
+//                 document.getElementById("foods" + i).innerHTML = food;
+//             }
+
+//         }
+//         ).catch(function (error) {
+//             console.log(error);
+
+//         });
+// }
 
 
 // function getApp() {
